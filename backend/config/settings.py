2fd017ba300cd636e84a4c15d6e102a3f10fd8aa
Sasha <kluzodota@gmail.com>
2026-07-29 11:49:27 +0300

    Add beautyaiservice.polandcentral.cloudapp.azure.com to ALLOWED_HOSTS

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -32,7 +32,11 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "beautyaiservice.polandcentral.cloudapp.azure.com",
+    "127.0.0.1",
+    "localhost",
+]
 
 # Application definition
 
