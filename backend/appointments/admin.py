e7de752bf9768849a5d6b0b0fd464b45835244c0
MiKPo4eLiK <stassojcuk@gmail.com>
2026-07-30 11:39:52 +0300

    Add admin panels for appointments, salons, beauty_service, reviews

--- backend/appointments/admin.py
+++ backend/appointments/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Appointment
+
+
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "client",
+        "master",
+        "salon",
+        "service",
+        "appointment_date",
+        "start_time",
+        "end_time",
+        "status",
+    )
+    list_filter = ("status", "appointment_date")
+    search_fields = ("client__email", "master__user__email")
